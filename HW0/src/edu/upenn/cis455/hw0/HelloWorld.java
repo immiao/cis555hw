d829505f8e046cc8f5ff60fa0152c6d69b09931d
Kaixiang Miao <miaok@seas.upenn.edu>
2017-01-22 21:41:32 -0500

    Solved HW0

--- HW0/src/edu/upenn/cis455/hw0/HelloWorld.java
+++ HW0/src/edu/upenn/cis455/hw0/HelloWorld.java
@@ -2,4 +2,7 @@ package edu.upenn.cis455.hw0;
 
 public class HelloWorld {
   // TODO: Add your code here
+	public static void main(String[] args) {
+		System.out.println("***Author: Kaixiang Miao (miaok)");
+	}
 }
