20aea8a7b6a968fe4f5b69ac36a64a959a80d95d
Andreas Haeberlen <ahae@cis.upenn.edu>
2017-02-23 17:45:15 -0500

    Added HW2

---
+++ HW2/src/edu/upenn/cis455/crawler/XPathCrawler.java
@@ -0,0 +1,11 @@
+package edu.upenn.cis455.crawler;
+
+
+public class XPathCrawler {
+	
+  public static void main(String args[])
+  {
+    /* TODO: Implement crawler */
+  }
+	
+}
---
+++ HW2/src/edu/upenn/cis455/servlet/XPathServlet.java
@@ -0,0 +1,33 @@
+package edu.upenn.cis455.servlet;
+
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class XPathServlet extends HttpServlet {
+	
+	/* TODO: Implement user interface for XPath engine here */
+	
+	/* You may want to override one or both of the following methods */
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+    {
+		/* TODO: Implement user interface for XPath engine here */
+	}
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) 
+	{
+		/* TODO: Implement user interface for XPath engine here */
+	}
+
+}
+
+
+
+
+
+
+
+
+
